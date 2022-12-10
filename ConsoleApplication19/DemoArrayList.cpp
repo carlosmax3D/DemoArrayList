e89ea72d95a18ab31431fb61011938ae66013e25
CARLOS ISRAEL ORTA ORTA <carlos.ortaort@uanl.edu.mx>
2022-12-10 16:02:00 -0600

    Add project files.

---
+++ ConsoleApplication19/DemoArrayList.cpp
@@ -0,0 +1,458 @@
+// DemoArrayList.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+// We created a template to generate a dynamic linked list as an array
+// We added Heapsort and Quicksort with the posibility to sort with a custom function
+template <typename T> 
+struct ArrayList {
+public:
+    // Custom exception in case of error at getting an element
+    class ArrayListIndexOutBound :public std::exception {
+        std::string error = "";
+        long long index = 0;
+    public:
+        ArrayListIndexOutBound() {
+            this->error.append("Array index out of bound");
+        }
+        ArrayListIndexOutBound(int index) {
+            this->error.append("Array index {").append(std::to_string(index)).append("} out of bound");
+        }
+        char* what() {
+            return error.c_str();
+        }
+    };
+    // We created an iterator for the linked list
+    template <typename E> struct ArrayListI {
+        E value;
+        ArrayListI* next = NULL;
+        ArrayListI* prev = NULL;
+        static ArrayListI <E>* newArrayListI(E value) {
+            ArrayListI < E >* n = new ArrayListI<E>;
+            if (n == NULL)
+                return NULL;
+            n->value = value;
+            n->next = NULL;
+            n->prev = NULL;
+            return n;
+        }
+        E& operator =(const E& b) {
+            this->value = b;
+        }
+        E& operator =(const ArrayListI<E>& b) {
+            this->value = b->value;
+        }
+    };
+    // Retreive the origin of the linked list
+    ArrayListI<T>* getOrigin() {
+        return origin;
+    }
+    // Retreive the end of the linked list
+    ArrayListI<T>* getLast() {
+        return last;
+    }
+    // Returns the iterator in the I position
+    ArrayListI<T>* getIterator(long long i) {
+        if (i >= size)
+            throw ArrayList<T>::ArrayListIndexOutBound(i);
+        int pointer =
+            abs(0 - i) > abs(size - i) ? (abs(size - i) > abs(itIndex - i) ? 3 : 2) : (abs(0 - i) > abs(itIndex - i) ? 3 : 1);
+        long long count = pointer == 1 ? 0 : (pointer == 2 ? size - 1 : itIndex);
+        ArrayListI<T>* iterator = pointer == 1 ? origin : (pointer == 2 ? last : it);
+        //    cout << (pointer == (byte)1 ? "origin" : (pointer == (byte)2 ? "last" : "itIndex")) << endl;
+        while (count < i && iterator != NULL) {
+            count++;
+            iterator = iterator->next;
+        }
+        while (count > i && iterator != NULL) {
+            count--;
+            iterator = iterator->prev;
+        }
+        it = iterator;
+        itIndex = count;
+        return iterator;
+    }
+    // Set a element in the I position and return the previous Element
+    T set(long long i, T value) {
+        ArrayListI<T>* iterator = getIterator(i);
+        T val = iterator->value;
+        iterator->value = value;
+        return val;
+    }
+    // Get the element in the I position
+    T get(long long i) {
+        ArrayListI <T>* iterator = getIterator(i);
+        return iterator->value;
+    }
+    // Add element at the end of the list
+    bool add(T value) {
+        ArrayList<T>::ArrayListI<T>* n =
+            ArrayList<T>::ArrayListI < T >::newArrayListI(value);
+        if (n == NULL)
+            return false;
+        origin = origin == NULL ? n : origin;
+        if (last != NULL)
+            last->next = n;
+        n->prev = last;
+        last = n;
+        size++;
+        itIndex = it == NULL ? 0 : itIndex;
+        it = it == NULL ? origin : it;
+        return true;
+    }
+    // Add element at the end of the list
+    bool push_back(T value) {
+        return add(value);
+    }
+    // Add element in front of the list
+    bool push_front(T value) {
+        ArrayList<T>::ArrayListI<T>* n =
+            ArrayList<T>::ArrayListI < T >::newArrayListI(value);
+        if (n == NULL)
+            return false;
+        last = last == NULL ? n : last;
+        if (origin != NULL)
+            origin->prev = n;
+        n->next = origin;
+        origin = n;
+        size++;
+        itIndex = it == NULL ? 0 : itIndex;
+        it = it == NULL ? origin : it;
+        return true;
+    }
+    // Delete an element from the list and return the value in the index
+    T erase(long long int i) {
+        ArrayListI<T>* iterator = getIterator(i);
+        T value = iterator->value;
+        if (iterator != NULL) {
+            ArrayListI<T>* next = iterator->next;
+            ArrayListI<T>* prev = iterator->prev;
+            if (prev != NULL)
+                prev->next = iterator->next;
+            if (next != NULL)
+                next->prev = iterator->prev;
+            if (iterator == origin)
+                origin = next;
+            if (iterator == last)
+                last = prev;
+            itIndex = iterator == it ? (itIndex - 1 < 0 ? 0 : itIndex - 1) : itIndex;
+            it = it == iterator ? (prev == NULL ? origin : prev) : it;
+            delete iterator;
+            size--;
+        }
+        return value;
+    }
+    // Overload the operator [] to get the elements as a list
+    T operator[](long long i) {
+        return this->get(i);
+    }
+    // Returns the size of the list
+    long long getSize() {
+        return size;
+    }
+    // A utility function to swap two index in the list
+    void swap(long long i, long long j) {
+        swap(getIterator(i), getIterator(j));
+    }
+    // A utility function to swap two iterators in the list
+    void swap(ArrayListI<T>* a, ArrayListI<T>* b) {
+        T t = a->value;
+        a->value = b->value;
+        b->value = t;
+    }
+
+    // Basic quicksort with default sorting
+    bool quicksort() {
+        quickSort(0, size - 1, NULL);
+        return true;
+    }
+    // Quicksort with custom sorting
+    bool quicksort(bool (*callback)(T value1, T value2)) {
+        quickSort(0, size - 1, callback);
+        return true;
+    }
+    // Basic heapsort with default sorting
+    bool heapsort() {
+        return heapsort(NULL);
+    }
+    // Heapsort with custom sorting
+    bool heapsort(bool (*callback)(T value1, T value2)) {
+        // Build heap (rearrange array)
+        for (long long i = size / 2 - 1; i >= 0; i--)
+            heapify(size, i, callback);
+        // One by one extract an element
+        // from heap
+        for (long long i = size - 1; i > 0; i--) {
+            // Move current root to end
+            swap(origin, getIterator(i));
+            // call max heapify on the reduced heap
+            heapify(i, 0, callback);
+        }
+        return true;
+    }
+    // Clear the list
+    void clear() {
+        it = origin;
+        while (it != NULL) {
+            origin = origin->next;
+            delete it;
+            it = origin;
+        }
+        size = 0;
+        itIndex = 0;
+        last = NULL;
+    }
+    // Search and return matches
+    ArrayList<T>* filter(T filter, bool (*callback)(T value, T filter)) {
+        if (callback == NULL || size < 1 || origin == NULL || last == NULL)
+            return NULL;
+        ArrayList<T>* ret = new ArrayList<T>;
+        ArrayListI<T>* iterator = origin, * iteratorE = last;
+        while (iterator != NULL) {
+            if (callback(iterator->value, filter))
+                ret->add(iterator->value);
+            if (iterator != iteratorE && callback(iteratorE->value, filter))
+                ret->add(iteratorE->value);
+            iteratorE = iteratorE->prev;
+            iterator = iterator->next;
+            if (iterator->prev == iteratorE || iterator->prev == iteratorE->next)
+                break;
+        }
+        if (ret->size < 1) {
+            delete ret;
+            ret = NULL;
+        }
+        return ret;
+    }
+    void toList(T* values, long long vsize) {
+        for (int i = 0; i < vsize; i++)
+            add(values[i]);
+    }
+    T* toArray() {
+        T* arr = new T[getSize()];
+        ArrayListI<T>* iterator = origin;
+        long long i = 0;
+        while (iterator != NULL) {
+            arr[i] = iterator->value;
+            iterator = iterator->next;
+            i++;
+        }
+        return arr;
+    }
+    static ArrayList<T>* arrayToList(T* values, long long vsize) {
+        ArrayList<T>* conv = new ArrayList<T>;
+        conv->toList(values, vsize);
+        return conv;
+    }
+private:
+    // First position in the list
+    ArrayListI<T>* origin = NULL;
+    // Size of the list
+    long long size = 0;
+    // Last position in the list
+    ArrayListI<T>* last = NULL;
+    // Main iterator for searching elements
+    ArrayListI<T>* it = NULL;
+    // Last index retreived in the iterator
+    long long itIndex = 0;
+
+    /* This function takes last element as pivot, places
+    the pivot element at its correct position in sorted
+    list, and places all smaller (smaller than pivot)
+    to left of pivot and all greater elements to right
+    of pivot */
+    long long partition(long long low, long long high, bool (*callback)(T value1, T value2)) {
+        //T pivot = get(high); // pivot
+        ArrayListI<T>* pivoti = getIterator(high);
+        long long i
+            = (low
+                - 1); // Index of smaller element and indicates
+        // the right position of pivot found so far
+
+        for (long long j = low; j <= high - 1; j++) {
+            // If current element is smaller than the pivot
+            ArrayListI<T>* ji = getIterator(j);
+            if ((callback != NULL && callback(ji->value, pivoti->value)) or (callback == NULL && sort_default(ji->value, pivoti->value))) {
+                i++; // increment index of smaller element
+                swap(getIterator(i), ji);
+            }
+        }
+        swap(getIterator(i + 1), pivoti);
+        return (i + 1);
+    }
+    /* The main function that implements QuickSort
+    low --> Starting index,
+    high --> Ending index
+    callback --> custom function for sorting
+    */
+    void quickSort(long low, long high, bool (*callback)(T value1, T value2)) {
+        if (low < high) {
+            /* pi is partitioning index, arr[p] is now
+            at right place */
+            long long pi = partition(low, high, callback);
+
+            // Separately sort elements before
+            // partition and after partition
+            quickSort(low, pi - 1, callback);
+            quickSort(pi + 1, high, callback);
+        }
+    }
+    // To heapify a subtree rooted with node i
+    // which is an index in the list.
+    // n is size of heap
+    void heapify(long long N, long long i, bool (*callback)(T value1, T value2)) {
+        // Initialize largest as root
+        long long largest = i;
+        // left = 2*i + 1
+        long long l = 2 * i + 1;
+        // right = 2*i + 2
+        long long r = 2 * i + 2;
+        // If left child is larger than root
+        ArrayListI<T>* li = l < N ? getIterator(l) : NULL, * ri = r < N ? getIterator(r) : NULL, * largesti = getIterator(largest);
+        if (l < N && ((callback != NULL && callback(largesti->value, li->value)) or (callback == NULL && sort_default(largesti->value, li->value)))) {
+            largest = l;
+            largesti = li;
+        }
+        // If right child is larger than largest
+        // so far
+        if (r < N && ((callback != NULL && callback(largesti->value, ri->value)) or (callback == NULL && sort_default(largesti->value, ri->value)))) {
+            largest = r;
+            largesti = ri;
+        }
+        // If largest is not root
+        if (largest != i) {
+            swap(getIterator(i), largesti);
+            // Recursively heapify the affected
+            // sub-tree
+            heapify(N, largest, callback);
+        }
+    }
+    // Default function for sorting
+    bool sort_default(T value1, T value2) {
+        return value1 > value2;
+    }
+};
+
+struct PERSONA {
+    char nombre[255] = {'\0'};
+    char curp[255] = { '\0' };
+    int edad = 0;
+    float peso = 0;
+    string getNombre() {
+        string s(nombre);
+        return s;
+    }
+    void setNombre(string n) {
+        strcpy_s(nombre, 255, n.c_str());
+    }
+    string getCurp() {
+        string s(curp);
+        return s;
+    }
+    void setCurp(string c) {
+        strcpy_s(curp, 255, c.c_str());
+    }
+    static PERSONA *newPersona(string nombre, string curp, int edad, int peso) {
+        PERSONA* nuevo = new PERSONA;
+        if (nuevo == NULL)
+            return NULL;
+        nuevo->setCurp(curp);
+        nuevo->setNombre(nombre);
+        nuevo->edad = edad;
+        nuevo->peso = peso;
+        return nuevo;
+    }
+};
+struct CALIFICACIONES {
+    float calificaciones[10] = { 0 };
+    char curp[255] = { '\0' };
+    void setCurp(string c) {
+        strcpy_s(curp, 255, c.c_str());
+    }
+    string getCurp() {
+        string s(curp);
+        return s;
+    }
+    static CALIFICACIONES* newCalificaciones(string curp) {
+        CALIFICACIONES* nuevo = new CALIFICACIONES;
+        if (nuevo == NULL)
+            return NULL;
+        nuevo->setCurp(curp);
+        return nuevo;
+    }
+};
+ArrayList<CALIFICACIONES*>c;
+ArrayList<PERSONA*>p;
+
+bool customSort(char value1, char value2) {
+    return value1 < value2;
+}
+
+bool customFilter(char value1, char value2) {
+    return value1 == value2;
+}
+
+bool filtroCurp(PERSONA* p1, PERSONA* p2) {
+    return _strcmpi(p1->curp, p2->curp) == 0;
+}
+bool calificacionPorNombre(CALIFICACIONES* c1, CALIFICACIONES* c2) {
+    PERSONA* curp1 = PERSONA::newPersona("", c1->curp, 0, 0);
+    ArrayList<PERSONA*> *p1 = p.filter(curp1, filtroCurp);
+    delete curp1;
+    if (p1 == NULL)
+        return false;
+    PERSONA* curp2 = PERSONA::newPersona("", c2->curp, 0, 0);
+    ArrayList<PERSONA*>* p2 = p.filter(curp2, filtroCurp);
+    delete curp2;
+    if (p2 == NULL)
+        return false;
+    bool result = (*p1)[0]->curp[0] > (*p2)[0]->curp[0];
+    p1->clear();
+    delete p1;
+    p2->clear();
+    delete p2;
+    return result;
+}
+
+int main(int argc, char** argv) {
+    try {
+        p.add(PERSONA::newPersona("Gilberto", "GILB941104HNLRRR04", 20, 65));
+        p.add(PERSONA::newPersona("Beatriz", "BEAT941104HNLRRR04", 20, 65));
+        p.add(PERSONA::newPersona("Ana", "ANAA941104HNLRRR04", 20, 65));
+        c.add(CALIFICACIONES::newCalificaciones("GILB941104HNLRRR04"));
+        c.add(CALIFICACIONES::newCalificaciones("BEAT941104HNLRRR04"));
+        c.add(CALIFICACIONES::newCalificaciones("ANAA941104HNLRRR04"));
+        c.quicksort(calificacionPorNombre);
+        cout << c.getSize() << endl;
+        ArrayList<char> v;
+        char test[] = { 'B', 'D', 'C', 'A', 'E', 'F', 'G', 'H' };
+        v.toList(test, 8);
+        ArrayList<char>* n = v.filter('A', customFilter);
+        if (n != NULL) {
+            cout << n->getSize() << endl;
+            for (int i = 0; i < n->getSize(); i++)
+                cout << "Filter: " << (*n)[i] << endl;
+            n->clear();
+            delete n;
+        } else cout << "NO RESULTS" << endl;
+        for (int i = 0; i < v.getSize(); i++)
+            cout << v[i] << ", ";
+        cout << endl;
+        //      v.erase(4);
+        v.heapsort(customSort);
+        //      v.quicksort(customSort);
+        for (int i = 0; i < v.getSize(); i++)
+            cout << v[i] << ", ";
+        cout << endl;
+        v.clear();
+    }
+    catch (...) {
+        cout << "\nError...\n";
+    }
+    return 0;
+}
